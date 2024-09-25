54c7cd7b9bb45d6c73847ee77dd98d89ef01a1e3
WongYC-66 <yengchien@hotmail.com>
2024-09-25 18:30:43 +0800

    temporarily disable output:export at next.config, to test at vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',   // // export as static sites html, disable when use dynamic (web hosting)
+    // output: 'export',   // // export as static sites html, disable when use dynamic (web hosting)
     images: {
         remotePatterns: [
             {
