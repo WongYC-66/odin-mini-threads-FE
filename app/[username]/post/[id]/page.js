91bc5c34d80243a7ede96bffb82534c877932394
WongYC-66 <yengchien@hotmail.com>
2024-09-25 18:10:37 +0800

    from fully static to ISR

--- app/[username]/post/[id]/page.js
+++ app/[username]/post/[id]/page.js
@@ -9,6 +9,14 @@ export default function PostPage(props) {
 }
 
 // for static build
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 10 seconds.
+export const revalidate = 10;
+// We'll prerender only the params from `generateStaticParams` at build time.
+// If a request comes in for a path that hasn't been generated,
+// Next.js will server-render the page on-demand.
+export const dynamicParams = true // or false, to 404 on unknown paths
+
 export async function generateStaticParams() {
     // params [ { username, id }]
     const { data } = await fetchDynamicParams('/next/posts')
